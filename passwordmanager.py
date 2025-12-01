a70ed20f0a170747f24b60d884a908fb76321255
wtfassdh <mixarvs@mail.ru>
2025-12-01 20:34:25 +0300

    добавил класс PasswordEntry, который отвечает за хранение записей. Содержит в себе service, username, password, entry id. Выводит информацию о записи с помощью display, для работы с бд to_dict преобразует данные в словарь, from_dict создает новый объект из словоря.

--- passwordmanager.py
+++ passwordmanager.py
@@ -1,1 +1,42 @@
-print('hellowrld')
+import sqlite3 
+from datetime import datetime
+
+class PasswordEntry:
+
+    def __init__(self, entry_id=None, service="", username="", password=""):
+        self.id = entry_id
+        self.service = service
+        self.username = username
+        self.password = password
+
+    #Строковое представление
+    def __str__(self):
+        return f"PasswordEntry(id={self.id}, service='{self.service}')"
+    
+    #Информация о записи
+    def display(self, show_password=False):
+        print(f"Сервис: {self.service}")
+        print(f"Логин: {self.username}")
+        if show_password:
+            print(f"Пароль: {self.password}")
+        else: 
+            print(f"Пароль: {'*' * len(self.password)}")
+
+    #Преобразование в словарь
+    def to_dict(self):
+        return {
+            'id' : self.id,
+            'service' : self.service,
+            'username' : self.username,
+            'password' : self.password
+        }
+    
+    @classmethod
+    #Объект из словоря
+    def from_dict(cls, data):
+        return cls(
+            entry_id=data.get('id'),
+            service=data.get('service', ''),
+            username=data.get('username', ''),
+            password=data.get('password', '')
+        )
