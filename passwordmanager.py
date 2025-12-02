7ab57a2a51f8c9f22b0db9b3a3de77285a748e23
wtfassdh <mixarvs@mail.ru>
2025-12-02 21:57:05 +0300

    для разработки сделал ветку develop, добавил ci.yml файл, а так же requirements-dev.txt, requirements.txt

--- passwordmanager.py
+++ passwordmanager.py
@@ -9,11 +9,11 @@ class PasswordEntry:
         self.username = username
         self.password = password
 
-    #Строковое представление
+    # Строковое представление
     def __str__(self):
         return f"PasswordEntry(id={self.id}, service='{self.service}')"
     
-    #Информация о записи
+    # Информация о записи
     def display(self, show_password=False):
         print(f"Сервис: {self.service}")
         print(f"Логин: {self.username}")
@@ -22,7 +22,7 @@ class PasswordEntry:
         else: 
             print(f"Пароль: {'*' * len(self.password)}")
 
-    #Преобразование в словарь
+    # Преобразование в словарь
     def to_dict(self):
         return {
             'id' : self.id,
@@ -32,7 +32,7 @@ class PasswordEntry:
         }
     
     @classmethod
-    #Объект из словоря
+    # Объект из словоря
     def from_dict(cls, data):
         return cls(
             entry_id=data.get('id'),
@@ -40,3 +40,22 @@ class PasswordEntry:
             username=data.get('username', ''),
             password=data.get('password', '')
         )
+
+class PasswordDatabase:
+    def __init__(self, db_path='passwords.db'):
+        self.db_path = db_path
+        self.connection = None
+        self._init_database() # Создание таблици при инициализации
+
+    def _init_database(self):
+        self._execute_query('''
+            CREATE TABLE IF NOT EXISTS passwords (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                service TEXT NOT NULL,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL,
+                created_at TIMESTAMP DEAFULT CURRENT_TIMESTAMP 
+            )
+        ''')
+
+    
