f9159ba98e9e78d2e792f71c38773d374594cc92
Matty2606 <130790822+Matty2606@users.noreply.github.com>
2026-01-10 01:03:13 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ from infrastructure.entrypoints.telegram.bot import main
 
 if __name__ == "__main__":
     asyncio.run(main())
+
+@dp.message_handler(commands=["start"])
+async def cmd_start(message: types.Message):
+    await message.reply("Бот работает!")
