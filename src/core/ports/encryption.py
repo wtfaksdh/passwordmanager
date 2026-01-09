16fd1e6d8afc0ce6fff549f3a0fe45e1e04128dc
Matty2606 <130790822+Matty2606@users.noreply.github.com>
2026-01-09 23:18:57 +0400

    Update encryption.py \\\

--- src/core/ports/encryption.py
+++ src/core/ports/encryption.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 from typing import List
 from abc import ABC, abstractmethod
-from core.domain import PasswordEntry, User, EncryptedPassword, CipherType
+from core.domain.entities.password_entry import PasswordEntry
+from core.domain.entities.user import User
+from core.domain.value_objects.encrypted_password import EncryptedPassword
+from core.domain.enums.cipher_type import CipherType
+
 
 class EncryptionService(ABC):
     @abstractmethod
